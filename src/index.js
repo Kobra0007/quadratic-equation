08797ae1604430c4db48ccd33f8d30f55b2d5c98
Kobra0007 <kobra007@github.com>
2018-02-24 04:56:29 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+   
+    let answer = [];
+    let arr = equation.replace(/\s/g, "").match(/[^\^]\d+/g);
+
+    let discriminant = Math.pow(arr[1], 2) - 4 * arr[0] * arr[2];
+    if(discriminant > 0) {
+        let rootDisc = Math.sqrt(discriminant);
+        answer[0] = Math.round(( -arr[1] - rootDisc) / (2 * arr[0]));
+        answer[1] = Math.round(( -arr[1] + rootDisc) / (2 * arr[0]));
+    }
+    return answer.sort( (a,b) =>{
+        return a - b ;
+    } );
+    
 }
+ 
